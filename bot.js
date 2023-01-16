446c5dd814b0460c31d2d216f64dfa104eef748c
fart burger <campbellgbaseball9@gmail.com>
2023-01-16 15:50:16 -0600

    interaction event fix

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ const {queryGameServerPlayer,queryGameServerInfo,queryMasterServer,REGIONS} = re
 const path = require('path')
 
 const client = new Client({
-    intents: [GatewayIntentBits.GuildMessages]
+    intents: [GatewayIntentBits.GuildMessages,GatewayIntentBits.GuildMembers]
 });
 client.commands = new Collection();
 const commandsPath = path.join(__dirname,'commands');
@@ -88,9 +88,23 @@ function playSound(link) {
 }
 
 
-client.on(Events.InteractionCreate, interaction => {
-    console.log(interaction);
-})
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(`Error executing ${interaction.commandName}`);
+		console.error(error);
+	}
+});
 
 client.on("messageCreate", (message) => {
     message.guild.members.fetch(client.user.id)
