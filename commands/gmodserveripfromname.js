2636187a4892f0efa0e093d2d56782c8dc87adc1
fart burger <campbellgbaseball9@gmail.com>
2023-01-16 18:10:19 -0600

    f

--- commands/gmodserveripfromname.js
+++ commands/gmodserveripfromname.js
@@ -18,7 +18,7 @@ module.exports = {
             const infoEmbed = new EmbedBuilder()
             .setTitle("Matching Servers")
             .setColor("Blurple")
-            .addFields({name:"Found Servers: ",value: `${servers}`})
+            .addFields({name:"Found Servers: ",value: JSON.stringify(servers,null,2)})
             await interaction.reply({embeds: [infoEmbed]})
         })
         .catch(e => {
